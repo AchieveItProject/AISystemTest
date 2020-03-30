5eefe8829325483c0742f16e4d4e29ba258b7e7f
Wu Wenhan <1020645987@qq.com>
2020-03-30 16:25:50 +0800

    Add more explorers

--- src/main/java/com/achieveit/systemtest/constant/Constant.java
+++ src/main/java/com/achieveit/systemtest/constant/Constant.java
@@ -6,7 +6,7 @@ package com.achieveit.systemtest.constant;
 public class Constant {
 
     public static String baseUrl="http://localhost:8080/";
-    // public static String baseTestUrl="http://47.100.187.197:4445/wd/hub";
-    public static String baseTestUrl="http://localhost:4445/wd/hub";
+     public static String baseTestUrl="http://47.100.187.197:4445/wd/hub";
+//    public static String baseTestUrl="http://localhost:4445/wd/hub";
 
 }
